cbde36587519ec2e269fdbe5bc46761d401d027b
NacereddineRebouh <llbl2153@gmail.com>
2021-07-30 15:45:02 +0100

    Add project files.

---
+++ Calculator/ButtonRessource.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace Calculator
+{
+    class ButtonRessource : ResourceDictionary
+    {
+        public ButtonRessource()
+        {
+            InitializeComponent();
+        }
+        private void Exit_Clicked(object sender, MouseEventArgs e)
+        {
+            ;
+        }
+        private void Full_Clicked(object sender, MouseEventArgs e)
+        {
+            Close();
+        }
+        private void Min_Clicked(object sender, MouseEventArgs e)
+        {
+            Close();
+        }
+    }
---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+        }
+
+
+        //style buttons
+    }
+}
