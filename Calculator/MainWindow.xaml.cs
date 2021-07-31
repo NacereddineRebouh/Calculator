091457aeba04c91c02240bbbff99a7d239773f66
NacereddineRebouh <llbl2153@gmail.com>
2021-07-31 19:07:21 +0100

    2021/07/31: Creating a Converter that switch the Maximize imagebutton !!  Bismillaaaaaaaaaaah  !!

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,16 +1,68 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Calculator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
+
     public partial class MainWindow : Window
     {
+
+
+
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = this;
+            this.DataContext = this;
+            string Test = "mew";
+            bool Max = true;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            meow2.Content = "f";
+            Button srcButton = e.Source as Button;
+            _ = MessageBox.Show(srcButton.Name);
+            switch (srcButton.Name)
+            {
+                case "Min":
+                    // Minimize Window
+                    meow2.Content = "1";
+                    WindowState = WindowState.Minimized;
+                    break;
+
+                case "Full":
+                    // FullSize/OriginalSize Window
+
+                    if (WindowState == WindowState.Maximized)
+                    {
+                        meow2.Content = "2";
+                        MainWindow.Max = false;
+                        //srcButton.FindName("image22") = new BitmapImage(new Uri("yor image uri"));
+                        WindowState = WindowState.Normal;
+
+
+                    }
+                    else
+                    {
+                        meow2.Content = "3";
+                        Max = true;
+                        WindowState = WindowState.Maximized;
+
+
+                    }
+
+                    break;
+
+                case "Exit":
+                    // Exit
+                    Close();
+                    break;
+                default:
+                    break;
+            }
+
 
         }
 
